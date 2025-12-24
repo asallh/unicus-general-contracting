d223ef3ab6e5e4dc76457d9edf338e75481e5513
asallh <ajaypalsallh@gmail.com>
2025-12-23 21:49:55 -0500

    Contact Banner added to the final page

--- apps/client-web/src/app/pages/projects/page.tsx
+++ apps/client-web/src/app/pages/projects/page.tsx
@@ -1,3 +1,4 @@
+import ContactBanner from "~/app/_components/ContactBanner";
 import Projects from "~/app/_components/projects";
 import { HydrateClient } from "~/trpc/server";
 
@@ -6,8 +7,10 @@ export default function ProjectsPage() {
     <HydrateClient>
       <div className="bg-brand-tertiary">
         <section className="container mx-auto px-4 py-12 sm:py-16 md:py-24 lg:py-32">
-          <h2 className="text-brand-accent pb-3 sm:pb-4 text-2xl sm:text-3xl md:text-4xl font-bold">Our Work</h2>
-          <p className="text-brand-accent text-base sm:text-lg md:text-xl leading-relaxed max-w-3xl">
+          <h2 className="text-brand-accent pb-3 text-2xl font-bold sm:pb-4 sm:text-3xl md:text-4xl">
+            Our Work
+          </h2>
+          <p className="text-brand-accent max-w-3xl text-base leading-relaxed sm:text-lg md:text-xl">
             With over <span className="font-bold">15 years of experience</span>{" "}
             building reliable projects, we have built retail stores, grocery
             marts, law offices and more. Click on a project to view more details
@@ -17,6 +20,10 @@ export default function ProjectsPage() {
       </div>
 
       <Projects />
+      <ContactBanner
+        bannerText="Ready for Results Like These? Let's Get Started."
+        buttons={["services", "contact us"]}
+      />
     </HydrateClient>
   );
 }
