a8ce87685c9adb924f7086acd54d92b7e6ae05fc
asallh <ajaypalsallh@gmail.com>
2025-12-29 21:52:37 -0500

    spell correct

--- packages/api/lib/s3.ts
+++ packages/api/lib/s3.ts
@@ -5,7 +5,7 @@ import {
 } from "@aws-sdk/client-s3";
 
 const s3Client = new S3Client({
-  region: process.env.AWS_REIGON,
+  region: process.env.AWS_REGION,
   credentials: {
     accessKeyId: process.env.AWS_ACCESS_KEY!,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
