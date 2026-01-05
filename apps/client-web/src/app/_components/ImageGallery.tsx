1d3bc7423f4d6dd93bfa4ee17fee779f3997031f
asallh <ajaypalsallh@gmail.com>
2026-01-04 20:50:16 -0700

    seeing if this fixed it

--- apps/client-web/src/app/_components/ImageGallery.tsx
+++ apps/client-web/src/app/_components/ImageGallery.tsx
@@ -4,7 +4,6 @@ import { useState } from "react";
 import { AnimatePresence, motion } from "framer-motion";
 import Image from "next/image";
 import { X } from "lucide-react";
-import { HydrateClient } from "~/trpc/server";
 
 interface ImageDisplaySection {
   url: string;
